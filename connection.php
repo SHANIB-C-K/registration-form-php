dd6fb636560d6d8d5e8c5ef475cd671fe68109a8
SHANIB C K <134039228+SHANIBCK@users.noreply.github.com>
2023-12-18 23:51:59 +0530

    Comment added in connection.php file

--- connection.php
+++ connection.php
@@ -1,9 +1,14 @@
 <?php
 
 $conn = mysqli_connect('localhost', 'root', '', 'registration');
+/*This is connecting to MySQL database localhost is my server name,
+root is a username of database, '' its password of database,
+registration is a name of database 
+*/
 
 if (!$conn) {
     die('Server doesnot connected');
+    //its check to server connect or not connect
 }
 
 ?>
